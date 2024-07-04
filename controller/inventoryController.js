baa0cce7b958b1f8a64144138dbf8d52c27076aa
Nessalyna <95633055+Nessalyna@users.noreply.github.com>
2024-07-04 23:16:55 +0000

    Inventory Update
    Added (Update and get single Inventory) controller and route

--- controller/inventoryController.js
+++ controller/inventoryController.js
@@ -12,6 +12,37 @@ const addInventory = (req, res) => {
   });
 };
 
+//Get a single inventory
+const getInventory = (req, res) => {
+  const { id } = req.params;
+  const query = 'SELECT * FROM inventory WHERE id = $1';
+  pool.query(query, [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (results.rows.length === 0) {
+      return res.status(404).json({ error: 'Inventory item not found' });
+    }
+    res.status(200).json(results.rows[0]);
+  });
+};
+
+// Update an inventory item
+const updateInventory = (req, res) => {
+  const { id } = req.params;
+  const {product_name, product_number, product_type, product_cost } = req.body;
+  const query = 'UPDATE inventory SET product_name = $1, product_number = $2, product_type = $3, product_cost = $4 WHERE id = $5';
+  pool.query(query, [product_name, product_number, product_type, product_cost, id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (results.rowCount === 0) {
+      return res.status(404).json({ error: 'Inventory item not found' });
+    }
+    res.status(200).json({ id, product_name, product_number, product_type, product_cost });
+  });
+};
+
 // Get all inventory items
 const getAllInventory = (req, res) => {
     const query = 'SELECT * FROM inventory';
@@ -42,5 +73,7 @@ const deleteInventory = (req, res) => {
   module.exports = {
     addInventory,
     getAllInventory,
-    deleteInventory
+    deleteInventory,
+    getInventory,
+    updateInventory
   };
